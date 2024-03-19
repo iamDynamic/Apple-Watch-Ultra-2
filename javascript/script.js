bfd05d365ed882a4486eb66d8070e1457e114bf2
= <=>
2024-03-19 14:48:18 +0200

    push website

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+var orange = document.getElementById("orange")
+var blue = document.getElementById("blue")
+var white = document.getElementById("white")
+var img = document.getElementById("img")
+
+orange.addEventListener("click",()=>{
+    img.src = "/images/Watch Ultra 2 (orane).png"
+})
+blue.addEventListener("click",()=>{
+    img.src = "/images/Watch Ultra 2 (blue).png"
+})
+white.addEventListener("click",()=>{
+    img.src = "/images/Watch Ultra 2 (white).png"
+})
