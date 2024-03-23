b5784f6610f75d2168f2821183a9e2b73abf7f7f
= <=>
2024-03-23 05:25:12 +0200

    add color property

--- javascript/script.js
+++ javascript/script.js
@@ -2,13 +2,16 @@ var orange = document.getElementById("orange")
 var blue = document.getElementById("blue")
 var white = document.getElementById("white")
 var img = document.getElementById("img")
-
+var color = document.getElementById("color")
 orange.addEventListener("click",()=>{
     img.src = "/images/Watch Ultra 2 (orane).png"
+    color.innerHTML = "orange"
 })
 blue.addEventListener("click",()=>{
+    color.innerHTML = "blue"
     img.src = "/images/Watch Ultra 2 (blue).png"
 })
 white.addEventListener("click",()=>{
     img.src = "/images/Watch Ultra 2 (white).png"
+    color.innerHTML = "white"
 })
